24ffaa14294b015b8c66f6497a06173cdd0f22ed
gunil0610 <gunil.na1@gmail.com>
2022-07-03 00:20:31 +0900

    feat: read deck from file

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -42,4 +43,16 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error{
 	err := ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 	return err
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - Log the error and return a call to newDeck()
+		// Option #2 - Log the error and entirely quit the program
+		fmt.Println("Error:",err) // Option #2
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs),",")
+	return deck(s)
 }
