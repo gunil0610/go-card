7e303d459e38d52c1e7e736bf583e571e83dbf59
gunil0610 <gunil.na1@gmail.com>
2022-07-03 00:38:04 +0900

    feat: function to shuffle deck

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 func main() {
-	// cards := newDeck()
-	// cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cards")
-	cards.print()	
+	cards := newDeck()
+	cards.print()
+	// cards = cards.shuffle()	
+	cards.shuffle()	
+	cards.print()
 }
